4dff41797de81bf741ca02fc40eaf2af3d1229fb
Nicolasd <nicoadiaz93@gmail.com>
2020-01-11 22:54:39 -0300

    Add project

---
+++ app/src/main/java/com/example/username/clase01/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.username.clase01;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtNumero;
+    private Button btnSumar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtNumero = findViewById(R.id.txtNumero);
+        btnSumar = findViewById(R.id.btnSumar);
+
+        btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero = Integer.parseInt(txtNumero.getText().toString()) + 1;
+                txtNumero.setText(String.valueOf(numero));
+            }
+        });
+    }
+}
